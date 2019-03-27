5aaefdac1d5a682c28335c0903a124a7fe17e227
Paulo <ph744915@gmail.com>
2019-03-26 21:43:59 -0300

    Projeto para o teste de implementação de um webservice

---
+++ ConversaoDinheiro/CompraVenda/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace CompraVenda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cmbMoedas = new System.Windows.Forms.ComboBox();
+            this.btnCotacao = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbMoedas
+            // 
+            this.cmbMoedas.FormattingEnabled = true;
+            this.cmbMoedas.Items.AddRange(new object[] {
+            "Dólar (Compra)",
+            "Dólar (Venda)",
+            "Euro (Compra)",
+            "Euro (Venda)",
+            "Iene (Compra)",
+            "Iene (Venda)",
+            "Dólar Canadense (Compra)",
+            "Dólar Canadense (Venda)"});
+            this.cmbMoedas.Location = new System.Drawing.Point(62, 51);
+            this.cmbMoedas.Name = "cmbMoedas";
+            this.cmbMoedas.Size = new System.Drawing.Size(121, 21);
+            this.cmbMoedas.TabIndex = 0;
+            // 
+            // btnCotacao
+            // 
+            this.btnCotacao.Location = new System.Drawing.Point(214, 34);
+            this.btnCotacao.Name = "btnCotacao";
+            this.btnCotacao.Size = new System.Drawing.Size(121, 52);
+            this.btnCotacao.TabIndex = 1;
+            this.btnCotacao.Text = "COTAÇÃO";
+            this.btnCotacao.UseVisualStyleBackColor = true;
+            this.btnCotacao.Click += new System.EventHandler(this.btnCotacao_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Moeda:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 129);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCotacao);
+            this.Controls.Add(this.cmbMoedas);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cotação Moedas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbMoedas;
+        private System.Windows.Forms.Button btnCotacao;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ConversaoDinheiro/CompraVenda/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompraVenda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCotacao_Click(object sender, EventArgs e)
+        {
+            /*Lista dos Codigos da Referencia "Moeda"
+            10813	Dólar (compra)
+            21619	Euro (venda)
+            21620	Euro (compra)
+            21621	Iene (venda)
+            21622	Iene (compra)
+            21623	Libra esterlina (venda)
+            21624	Libra esterlina (compra)
+            21625	Franco Suíço (venda)
+            21626	Franco Suíço (compra)
+            21627	Coroa Dinamarquesa (venda)
+            21628	Coroa Dinamarquesa (compra)
+            21629	Coroa Norueguesa (venda)
+            21630	Coroa Norueguesa (compra)
+            21631	Coroa Sueca (venda)
+            21632	Coroa Sueca (compra)
+            21633	Dólar Australiano (venda)
+            21634	Dólar Australiano (compra)
+            21635	Dólar Canadense (venda)
+            21636	Dólar Canadense (compra)*/
+
+            string moeda = cmbMoedas.Items[cmbMoedas.SelectedIndex].ToString();
+            int codMoeda = 1;
+
+            if (moeda == "Dólar (Compra)")
+            {
+                codMoeda = 10813;
+
+            }
+            else if (moeda == "Dólar (Venda)")
+            {
+                codMoeda = 1;
+            }
+            else if (moeda == "Euro (Compra)")
+            {
+                codMoeda = 21620;
+            }
+            else if (moeda == "Euro (Venda)")
+            {
+                codMoeda = 21619;
+            }
+            else if (moeda == "Iene (Compra)")
+            {
+                codMoeda = 21622;
+            }
+            else if (moeda == "Iene (Venda)")
+            {
+                codMoeda = 21621;
+            }
+            else if (moeda == "Dólar Canadense (Compra)")
+            {
+                codMoeda = 21635;
+            }
+            else if (moeda == "Dólar Canadense (Venda))")
+            {
+                codMoeda = 21636;
+            }
+            else
+            {
+                MessageBox.Show("Erro !!!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            //pegar o valor da moeda na referencia "Moeda"
+            var service = new Moeda.FachadaWSSGSClient();
+            var cotacao = service.getUltimoValorVO(codMoeda);
+            string valor = (cotacao.ultimoValor.svalor).ToString();
+            MessageBox.Show("Valor: " + valor, "Cotação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            
+        }
+
+    }
+}
